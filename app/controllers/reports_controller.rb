7416461a5e83e1f4ac6097b60aa2c2ffce2a3386
anjanamohanty <anjana.j.mohanty@gmail.com>
2016-03-23 00:14:35 -0400

    Parse CSV and create db rows

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -20,6 +20,10 @@ class ReportsController < ApplicationController
     CSV.open(params['csv_file'].tempfile, headers: true) do |c|
       c.each do |line|
         @rows << line['Assembly Name']
+        a = Assembly.find_by_name(line['Assembly Name']) || Assembly.create!(name: line['Assembly Name'], run_on: Time.now.to_date)
+        s = Sequence.create!(assembly_id: a.id, dna: line['Sequence'], quality: line['Sequence Quality'])
+        g = Gene.create!(sequence_id: s.id, dna: line["Gene Sequence"], starting_position: line["Gene Starting Position"], direction: line["Gene Direction"])
+        h = Hit.create!(subject_id: g.id, subject_type: "Gene", match_gene_name: line["Hit Name"], match_gene_dna: line["Hit Sequence"], percent_similarity: line['Hit Similarity'])
       end
     end
   end
