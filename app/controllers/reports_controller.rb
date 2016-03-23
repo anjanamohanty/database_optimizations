57b41d7dac5d50085574e756626fd2285329e5a6
anjanamohanty <anjana.j.mohanty@gmail.com>
2016-03-22 21:04:36 -0400

    Update controller efficiency and add search functionality. No partial searches right now

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,24 +2,29 @@ class ReportsController < ApplicationController
   helper_method :memory_in_mb
 
   def all_data
+    @name = params[:name]
+    @start_time = Time.now
+    @assembly = Assembly.find_by_name(params[:name])
+    @hits = @assembly.hits.order(percent_similarity: :desc)
+    @memory_used = memory_in_mb
+  end
+
+  def search_view
+  end
+
+  def search
+    @name = params[:search]
     @start_time = Time.now
 
-    @sequences = []
-    @genes = []
-    @hits = []
-    @assembly = Assembly.find_by_name(params[:name])
-    @assembly.sequences.each do |s|
-      @sequences << s
-      s.genes.each do |g|
-        @genes << g
-        g.hits.each do |h|
-          @hits << h
-        end
-      end
-    end
-    @hits.sort! {|a, b| b.percent_similarity <=> a.percent_similarity}
+    @hits = Hit.joins("JOIN genes ON genes.id = hits.subject_id AND hits.subject_type = 'Gene'")
+        .joins("JOIN sequences ON sequences.id = genes.sequence_id")
+        .joins("JOIN assemblies ON assemblies.id = sequences.assembly_id")
+        .where("assemblies.name LIKE ? OR genes.dna LIKE ? OR hits.match_gene_name LIKE ?",
+            params[:search], params[:search], params[:search])
+        .order("hits.percent_similarity DESC")
 
     @memory_used = memory_in_mb
+    render 'all_data'
   end
 
   private def memory_in_mb
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'reports#all_data'
 
+  get 'search' => 'reports#search_view'
+  post 'search' => 'reports#search'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
