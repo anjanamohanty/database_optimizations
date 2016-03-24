7d6faf06a443822a1c2a13a980460ecdd7d8c924
anjanamohanty <anjana.j.mohanty@gmail.com>
2016-03-24 08:24:27 -0400

    Move csv generation code to background process

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -10,13 +10,7 @@ class ReportsController < ApplicationController
     @hits = @assembly.hits.order(percent_similarity: :desc)
     @memory_used = memory_in_mb
 
-    file_path = Rails.root.join("reports", "report_#{@name}_#{@start_time}.csv")
-    CSV.open(file_path, "w") do |csv|
-      csv << ["Matching Gene Name", "Matching DNA", "Percent Similarity"]
-      @hits.each do |h|
-        csv << [h.match_gene_name, h.match_gene_dna, h.percent_similarity]
-      end
-    end
+    MakeReportJob.perform_later(@name)
   end
 
   def search_view
---
+++ app/jobs/make_report_job.rb
@@ -0,0 +1,17 @@
+class MakeReportJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(*args)
+    start_time = Time.now
+    assembly = Assembly.find_by_name(args[0])
+    hits = assembly.hits.order(percent_similarity: :desc)
+
+    file_path = Rails.root.join("reports", "report_#{args[0]}_#{start_time}.csv")
+    CSV.open(file_path, "w") do |csv|
+      csv << ["Matching Gene Name", "Matching DNA", "Percent Similarity"]
+      hits.each do |h|
+        csv << [h.match_gene_name, h.match_gene_dna, h.percent_similarity]
+      end
+    end
+  end
+end
